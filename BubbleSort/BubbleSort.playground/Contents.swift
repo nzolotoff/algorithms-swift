d93028e402228321bb3ab968d4e6f70031228266
nzolotoff <nikita.zolotov37@yandex.ru>
2025-02-01 11:50:28 +0300

    add bubble sort

---
+++ BubbleSort/BubbleSort.playground/Contents.swift
@@ -0,0 +1,4 @@
+var numbers = [1, 5, 3, 9, 7]
+
+bubbleSort(array: numbers, isInIncreasingOrder: <) // gives [1, 3, 5, 7, 9]
+bubbleSort(array: numbers, isInIncreasingOrder: >) // gives [9, 7, 5, 3, 1]
---
+++ BubbleSort/BubbleSort.swift
@@ -0,0 +1,25 @@
+//
+//  BubbleSort.swift
+//  
+//
+//  Created by Nikita Zolotov on 01.02.2025.
+//
+
+import Foundation
+
+public func bubbleSort<T: Comparable>(
+    array: [T],
+    isInIncreasingOrder: (T, T) -> Bool
+) -> [T] {
+    var result = array
+    
+    for i in 0..<result.count {
+        for j in 0..<result.count - i - 1 {
+            if !isInIncreasingOrder(result[j], result[j+1]) {
+                result.swapAt(j, j+1)
+            }
+        }
+    }
+    
+    return result
+}
